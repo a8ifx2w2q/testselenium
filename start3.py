31083b1674b5e96cb0cbbe0054d664b2f3fd9ba8
t55xddb4w <85150156+t55xddb4w@users.noreply.github.com>
2021-06-01 20:09:03 +0530

    Create start3.py

---
+++ start3.py
@@ -0,0 +1,23 @@
+import time
+import os
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+weburl = os.getenv('WEB_URL')
+
+check=1
+while(check>0):
+    gChromeOptions = webdriver.ChromeOptions()
+    gChromeOptions.add_argument("window-size=1920x1480")
+    gChromeOptions.add_argument("disable-dev-shm-usage")
+    gChromeOptions.add_argument("incognito")
+    gDriver = webdriver.Chrome(
+        chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
+    )
+    
+    gDriver.get(weburl)
+    time.sleep(600)
+    gDriver.close()
+    print("Times Run (incognito) = ", check)
+    check=check+1
