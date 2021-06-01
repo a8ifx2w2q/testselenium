fc90363ac7c89fcc97bc5801f66e89c5d0104c26
t55xddb4w <85150156+t55xddb4w@users.noreply.github.com>
2021-06-01 20:11:49 +0530

    Update start2.py

--- start2.py
+++ start2.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 weburl = os.getenv('WEB_URL')
-
+time.sleep(10)
 check=1
 while(check>0):
     gChromeOptions = webdriver.ChromeOptions()
